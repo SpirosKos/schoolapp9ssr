2ceac53dae6e64e8000b3dc96aa948d227823ddc
sprs <spiroskos9@gmail.com>
2026-02-05 23:11:23 +0200

    feat: enable JPA and define AbstractEntity and Teacher

--- src/main/java/gr/aueb/cf/schoolapp/model/AbstractEntity.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/AbstractEntity.java
@@ -1,28 +1,31 @@
-//package gr.aueb.cf.schoolapp.model;
-//
-//import jakarta.persistence.Column;
-//import jakarta.persistence.EntityListeners;
-//import jakarta.persistence.MappedSuperclass;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import org.springframework.data.annotation.CreatedDate;
-//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-//
-//import java.time.Instant;
-//
-//@MappedSuperclass
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@EntityListeners(AuditingEntityListener.class)
-//public abstract class AbstractEntity {
-//
-//    @CreatedDate
-//    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "DATETIME")
-//    private Instant createdAt;          // Instant = UTC always
-//
-//    private Instant updatedAt;
-//}
+package gr.aueb.cf.schoolapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "DATETIME")
+    private Instant createdAt;          // Instant = UTC always
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME")
+    private Instant updatedAt;
+}
