c7c8c3aa71d82688a5e73c11581fa4514cdd8f7b
sprs <spiroskos9@gmail.com>
2026-02-17 20:36:20 +0200

    chore: get roles for showing in combo box

---
+++ src/main/java/gr/aueb/cf/schoolapp/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.schoolapp.repository;
+
+import gr.aueb.cf.schoolapp.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+//@Repository   since we extends JpaRepository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    List<Role> findByOrderByNameAsc();
+}
